6821e936d3cd9b92ef26649a09e77c10037e4487
XiXinping <redbinary@protonmail.com>
2025-05-25 00:40:57 -0700

    Added basic code to connect to WiFi

---
+++ src/main.c
@@ -0,0 +1,134 @@
+#include <esp_chip_info.h>
+#include <esp_flash.h>
+#include <esp_http_server.h>
+#include <esp_log.h>
+#include <esp_netif.h>
+#include <esp_system.h>
+#include <esp_wifi.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <nvs_flash.h>
+#include <sdkconfig.h>
+#include <stdio.h>
+#include <string.h>
+
+static const char *TAG = "main";
+
+// Handle GET requests
+esp_err_t get_handler(httpd_req_t *request) {
+    const char *response = "Hello, ESP32!";
+    httpd_resp_send(request, response, strlen(response));
+    return ESP_OK;
+}
+
+void start_webserver() {
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.server_port = 80;
+
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_uri_t uri = {
+            .uri = "/",
+            .method = HTTP_GET,
+            .handler = get_handler,
+            .user_ctx = NULL,
+        };
+        httpd_register_uri_handler(server, &uri);
+    }
+}
+
+void print_ip_address() {
+    esp_netif_ip_info_t ip_info;
+    esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+    if (netif != NULL) {
+        esp_netif_get_ip_info(netif, &ip_info);
+        printf("IP Address: " IPSTR "\n", IP2STR(&ip_info.ip));
+    } else {
+        printf("Failed to get netif handle!\n");
+    }
+}
+
+void wifi_init() {
+    // Initialize Wi-Fi driver
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+        err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // Try to erase the flash. Abort if there's an error
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    // Configure Wi-Fi
+    wifi_init_config_t init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&init_cfg));
+
+    // Go into station (regular device on network) mode
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+    // Set SSID and password
+    wifi_config_t wifi_config = {
+        .sta =
+            {
+                .ssid = "Noisebridge",
+                .password = "noisebridge",
+            },
+    };
+
+    // Apply the config
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    // Start the Wi-Fi module and connect to the network in the config
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "Connecting to Wi-Fi...");
+    while (true) {
+        esp_wifi_connect();
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        wifi_ap_record_t ap_info;
+        if (esp_wifi_sta_get_ap_info(&ap_info) == ESP_OK) {
+            ESP_LOGI(TAG, "Connected to %s", ap_info.ssid);
+            return;
+
+        } else {
+            ESP_LOGE(TAG, "Failed to connect to Wi-Fi!");
+        }
+    }
+}
+
+void app_main() {
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ", CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154)
+               ? ", 802.15.4 (Zigbee/Thread)"
+               : "");
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
+
+    // printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    //        (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded"
+    //                                                      : "external");
+    //
+    // printf("Minimum free heap size: %" PRIu32 " bytes\n",
+    //        esp_get_minimum_free_heap_size());
+    //
+
+    wifi_init();
+    printf("Initialized Wi-Fi!\n");
+    // start_webserver();
+}
